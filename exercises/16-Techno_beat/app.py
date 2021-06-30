8b4e37d99db93f000d3453d64999c100bfb56b32
fabianchs <fabichasola@hotmail.com>
2021-06-30 01:59:59 +0000

    all exercises solved!

--- exercises/16-Techno_beat/app.py
+++ exercises/16-Techno_beat/app.py
@@ -3,13 +3,13 @@ def lyrics_generator(lyric):
     final_string=""
     for number in lyric:
         if number==0:
-            final_string=final_string+" Boom"
+            final_string=final_string+"Boom "
             one_counter=0
         elif number==1:
-            final_string=final_string+" Drop the base"
+            final_string=final_string+"Drop the base "
             one_counter+=1
         if one_counter==3:
-            final_string=final_string+" !!!Break the base!!!"   
+            final_string=final_string+"!!!Break the base!!! "   
     return final_string
 
 # Your code go above, nothing to change after this line:
